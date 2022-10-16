8d065308be1acefbb449889a4fbd16ad10fb8664
Carlos Lombardi <carlombardi@gmail.com>
2022-10-16 21:11:53 +0200

    se agrega post para subir un PDF a Google Cloud Storage, se agregan comentarios

--- index.js
+++ index.js
@@ -4,14 +4,19 @@ const bodyParser = require("body-parser");
 const express = require('express')
 const _ = require('lodash');
 const cors = require("cors");
+const multer  = require('multer');
+const { Storage } = require('@google-cloud/storage');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// multer config
+const multerStorage = multer.memoryStorage();
+const multerFileHandler = multer({ storage: multerStorage });
 
-const { Storage } = require('@google-cloud/storage');
+// Google Storage config
 const gcs = new Storage({
     projectId: "biblapp-ff6b8",
     keyFilename: "./biblapp-ff6b8-firebase-adminsdk-yd0y2-f66e718c5b.json"
@@ -29,12 +34,13 @@ async function fetchFileFromGoogleStorage(filename) {
     return fileContents[0];
 }
 
+// una ruta de prueba
 app.get('/hello', (request, response) => {
     response.status(200);
     response.send("hola terricolas");
 });
 
-
+// obtiene una imagen desde Google Cloud Storage y la envía en la response
 app.get('/image', async (request, response) => {
     const downloadedImageFile = await fetchFileFromGoogleStorage('justImages/guanaco-barrancas-coloradas.jpg');
     response.status(200);
@@ -42,13 +48,46 @@ app.get('/image', async (request, response) => {
     response.send(downloadedImageFile);
 });
 
+// obtiene un documento PDF desde Google Cloud Storage y lo envía en la response
 app.get('/pdf', async (request, response) => {
-    const downloadedPdfFile = await fetchFileFromGoogleStorage('pdfs/Borges Jorge - Ficciones.pdf');
+    const filename = `pdfs/${request.query.filename || 'Borges Jorge - Ficciones'}.pdf`
+    const downloadedPdfFile = await fetchFileFromGoogleStorage(filename);
     response.status(200);
     response.type('application/pdf');
     response.send(downloadedPdfFile);
 });
 
-app.listen(process.env.PORT || 3123, null, null, () => console.log("Example app started"))
+// upload de un documento pdf (pero creo que sirve para cualquier archivo)
+// se espera un request multipart con un file llamado uploadedPdf, y en la parte de texto un atributo filename
+// (sin la extensión PDF, que se la pone el código del endpoint)
+app.post('/pdf', multerFileHandler.single('uploadedPdf'), async (request, response) => {
+    // observo el request
+    console.log(Object.keys(request.file));
+    console.log({ originalname: request.file.originalname, mimetype: request.file.mimetype });
+    console.log(request.body.filename);
 
+    // implemento el upload
+    const newFilePath = `pdfs/${request.body.filename}.pdf`;
+
+    // paso 1 - se crean el archivo, y el stream donde se va a inyectar el contenido
+    const blob = storage.file(newFilePath);
+    const blobStream = blob.createWriteStream();
+
+    // paso 2 - se inyecta el contenido
+    // a mí tampoco me gusta cómo queda el código, pero no encontré alternativa
+    // cfr la documentación - https://cloud.google.com/appengine/docs/flexible/using-cloud-storage?tab=node.js#top
+    blobStream.on('error', err => {
+        response.status(500);
+        response.json({ ok: false, errorDescription: err });
+    });
+    
+    blobStream.on('finish', () => {
+        response.status(200);
+        response.json({ ok: true, path: newFilePath });
+    });
+    
+    blobStream.end(request.file.buffer);
+});
+
+app.listen(process.env.PORT || 3123, null, null, () => console.log("Example app started"))
 
